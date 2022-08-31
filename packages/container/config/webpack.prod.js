cb844d7cbd18fe11331b15c9a04d417ade154c0a
Miquel <miquellopezavila@gmail.com>
2022-08-31 13:24:18 +0200

    Updated yml

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: '/container/latest/'
+        publicPath: '/container/latest/',
     },
     plugins: [
         new ModuleFederationPlugin({
