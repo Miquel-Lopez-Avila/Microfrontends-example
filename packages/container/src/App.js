d4e4f2781f85265b011423bed3a0fc0fb9ebd906
Miquel <miquellopezavila@gmail.com>
2022-09-01 10:10:07 +0200

    deploy marketing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 
 const Application = () => {
     return (
-        <div>
-            hi there!!!!
-            <hr/>
-            <MarketingApp />
-        </div>
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
+        </BrowserRouter>
     )
 }
 
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma'
+})
+
 const Application = () => {
     return (
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <Router>
                     <Switch>
                         <Route exact path={'/pricing'} component={Pricing} />
