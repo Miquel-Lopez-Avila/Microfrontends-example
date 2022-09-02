e63a09ab468748a92ce89bf5fac94fa5109550f7
Miquel <miquellopezavila@gmail.com>
2022-09-02 12:30:42 +0200

    deploy auth

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-// Mount function to start up the app
 const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
   const history =
     defaultHistory ||
@@ -18,18 +17,14 @@ const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
   ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
 
   return {
-    onParentNavigate({ pathname: nextPathname }) {
+    onParentNavigate: ({ pathname: nextPathname }) => {
       const { pathname } = history.location;
 
-      if (pathname !== nextPathname) {
-        history.push(nextPathname);
-      }
+      if (pathname !== nextPathname) history.push(nextPathname);
     },
   };
 };
 
-// If we are in development and in isolation,
-// call mount immediately
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_auth-dev-root');
 
@@ -38,6 +33,4 @@ if (process.env.NODE_ENV === 'development') {
   }
 }
 
-// We are running through container
-// and we should export the mount function
 export { mount };
