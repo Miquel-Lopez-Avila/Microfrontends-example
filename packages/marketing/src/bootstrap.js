909057dc6d7906158274aeee2221741201ce649b
Miquel <miquellopezavila@gmail.com>
2022-09-02 13:11:56 +0200

    deploy dashboard

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-// Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   const history =
     defaultHistory ||
@@ -28,8 +27,6 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   };
 };
 
-// If we are in development and in isolation,
-// call mount immediately
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
@@ -38,6 +35,4 @@ if (process.env.NODE_ENV === 'development') {
   }
 }
 
-// We are running through container
-// and we should export the mount function
 export { mount };
